7ee561d4acf7cf6b848a3e3fc89f4457ef1adeba
AAA <aaa@qq.com>
2020-05-17 06:19:07 +0800

    添加了$.ajaxSetup全局设置

--- admin/js/index.js
+++ admin/js/index.js
@@ -4,9 +4,9 @@ $(function () {
     type: 'get',
     // url: 'http://localhost:8080/api/v1/admin/user/info',
     url:BigNew.user_info,
-    headers: {
-      'Authorization': localStorage.getItem('token')
-    },
+    // headers: {
+    //   'Authorization': localStorage.getItem('token')
+    // },
     success: function (res) {
       console.log(res)
       $('.user_info span i').text(res.data.nickname)
